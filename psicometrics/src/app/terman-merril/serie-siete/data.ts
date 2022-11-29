baa1b82447eddfe108486e3f39a44ee5e4d2b8bd
alexocay <alexoa97@hotmail.com>
2022-11-28 21:59:21 -0600

    update

---
+++ psicometrics/src/app/terman-merril/serie-siete/data.ts
@@ -0,0 +1,36 @@
+const data = [{
+    question:{id:1, text:'1. 1. ABRIGO es a USAR como PAN es a:'}, answers: [{id:1, name:'Comer'}, {id:2, name:'Hambre'}, {id:3, name:'Agua'}, {id:4, name:'Cocinar'}]
+}, {
+    question:{id:2, text:'2. SEMANA es a MES como MES es a :'}, answers: [{id:1, name:'Año'}, {id:2, name:'Hora'}, {id:3, name:'Minuto'}, {id:4, name:'Siglo'}]
+}, {
+    question:{id:3, text:'3. LEON es a ANIMAL como ROSA es a :'}, answers: [{id:1, name:'Olor'}, {id:2, name:'Hoja'}, {id:3, name:'Planta'}, {id:4, name:'Espina'}]
+},{
+    question: { id:4, text:'4. LIBERTAD es a INDEPENDENCIA como CAUTIVERIO es a :'}, answers: [{id:1, name:'Negro'}, {id:2, name:'Esclavitud'}, {id:3, name:'Libre'}, {id:4, name:'Sufrir'}],
+},{
+    question: { id:5, text:'5. DECIR es a DIJO como ESTAR es a :'}, answers: [{id:1, name:'Cantar'}, {id:2, name:'Estuvo'}, {id:3, name:'Hablando'}, {id:4, name:'Cantó'}],
+},{ 
+    question: { id:6, text:'6. LUNES es a MARTES como VIERNES es a :'}, answers: [{id:1, name:'Semana'}, {id:2, name:'Jueves'}, {id:3, name:'Día'},  {id:4, name:'Sábado'}],
+},{ 
+    question: { id:7, text:'7. PLOMO es a PESADO como CORCHO es a :'}, answers: [{id:1, name:'Botella'}, {id:2, name:'Peso'}, {id:3, name:'Ligero'},  {id:4, name:'Flotar'}],
+},{ 
+    question: { id:8, text:'8. La guillotina causa:'}, answers: [{id:1, name:'Muerte'}, {id:2, name:'Enfermedad'}, {id:3, name:'Fiebre'},  {id:4, name:'Malestar'}],
+},{ 
+    question: { id:9, text:'9. La grúa se usa para:'}, answers: [{id:1, name:'Perforar'}, {id:2, name:'Cortar'}, {id:3, name:'Levantar'},  {id:4, name:'Exprimir'}],
+},{ 
+    question: { id:10, text:'10. Una figura de seis lados se llama:'}, answers: [{id:1, name:'Pentágono'}, {id:2, name:'Paralelogramo'}, {id:3, name:'Hexágono'},  {id:4, name:'Trapecio'}],
+},{ 
+    question: { id:11, text:'11. El Kilowatt mide:'}, answers: [{id:1, name:'Lluvia'}, {id:2, name:'Viento'}, {id:3, name:'Electricidad'},  {id:4, name:'Presión'}],
+},{ 
+    question: { id:12, text:'12. La pauta se usa en:'}, answers: [{id:1, name:'Agricultura'}, {id:2, name:'Música'}, {id:3, name:'Fotografía'},  {id:4, name:'Estenografía'}],
+},{ 
+    question: { id:13, text:'13. Las esmeraldas son:'}, answers: [{id:1, name:'Azules'}, {id:2, name:'Verdes'}, {id:3, name:'Rojas'},  {id:4, name:'Amarillas'}],
+},{ 
+    question: { id:14, text:'14. El metro es aproximadamente igual a:'}, answers: [{id:1, name:'Pie'}, {id:2, name:'Pulgada'}, {id:3, name:'Yarda'},  {id:4, name:'Milla'}],
+},{ 
+    question: { id:15, text:'15. Las esponjas se obtienen de:'}, answers: [{id:1, name:'Animales'}, {id:2, name:'Hierbas'}, {id:3, name:'Bosques'},  {id:4, name:'Minas'}],
+},{ 
+    question: { id:16, text:'16. Fraude es un término utilizado en:'}, answers: [{id:1, name:'Medicina'}, {id:2, name:'Teología'}, {id:3, name:'Leyes'},  {id:4, name:'Pedagogía'}],
+}
+]
+
+export default data;
